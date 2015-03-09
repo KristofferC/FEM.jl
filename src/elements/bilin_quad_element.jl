82453cb4b759903491a7d5017c857307770fc65c
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-03-10 00:26:53 +0100

    Add bilinear quadraterial element

---
+++ src/elements/bilin_quad_element.jl
@@ -0,0 +1,46 @@
+immutable BilinQuad <: Element
+    vertices::Vector{Int}
+    gps::Vector{GaussPoint}
+    n_dofs::Int
+    n::Int
+    interp::LinTrigInterp
+end
+
+
+function BilinQuad(vertices::Vector{Int}, n)
+
+    n_dofs = 8
+
+    p = 1 / sqrt(3)
+    gps = [GaussPoint([-p; -p], 1.0);
+           GaussPoint([ p; -p], 1.0)
+           GaussPoint([ p;  p], 1.0)
+           GaussPoint([-p;  p], 1.0)]
+
+
+    interp = BilinQuadInterp()
+
+    BilinQuad(vertices, gps, n_dofs, n, interp)
+end
+
+
+function doftypes(elem::LinTrig, vertex::Int)
+    return [Du(), Dv()]
+end
+
+
+function Bmatrix(elem::LinTrig, gp::GaussPoint, nodes::Vector{Node})
+    dNdx = dNdxmatrix(elem.interp, gp.local_coords, elem.vertices, nodes)
+    B = zeros(4, 8)
+
+    for i in 1:4
+        B[1, 2 * i - 1] = dNdx[i, 1]
+        B[2, 2 * i]     = dNdx[i, 2]
+
+        B[4, 2 * i - 1] = dNdx[i, 2]
+        B[4, 2 * i]     = dNdx[i, 1]
+    end
+    return B
+end
+
+
---
+++ src/interpolators/bilin_quad_interp.jl
@@ -0,0 +1,32 @@
+immutable BilinQuadInterp <: Interpolator end
+
+# Shape functions in local coordinates
+function Nvec(interp::BilinQuadInterp, loc_coords::Vector{Float64})
+
+    ξ = loc_coords[1]
+    η = loc_coords[2]
+
+    N = [(1.0 + ξ) * (1.0 + η) * 0.25;
+         (1.0 - ξ) * (1.0 + η) * 0.25;
+         (1.0 - ξ) * (1.0 - η) * 0.25;
+         (1.0 + ξ) * (1.0 - η) * 0.25]
+
+    return N
+end
+
+
+function dNmatrix(interp::BilinQuadInterp, loc_coords::Vector{Float64})
+    ξ = loc_coords[1]
+    η = loc_coords[2]
+
+    # Derivative w.r.t
+    #              ξ                     η
+    dN = [[ 0.25 * (1.0 + η)     0.25 * (1.0 + ξ)];
+          [-0.25 * (1.0 + η)     0.25 * (1.0 - ξ)];
+          [-0.25 * (1.0 - η)    -0.25 * (1.0 - ξ)];
+          [ 0.25 * (1.0 - η)    -0.25 * (1.0 + ξ)]]
+    return dN
+end
+
+
+
