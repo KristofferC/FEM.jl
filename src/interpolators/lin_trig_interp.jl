688c086b8459f8a857863df5ebf374d8bd123582
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-03-10 00:31:44 +0100

    Move gneric interp functions to abstract type

--- src/interpolators/lin_trig_interp.jl
+++ src/interpolators/lin_trig_interp.jl
@@ -13,16 +13,6 @@ function Nvec(interp::LinTrigInterp, loc_coords::Vector{Float64})
     return N
 end
 
-function dNdxmatrix(interp::LinTrigInterp, local_coords::Vector{Float64},
-                    vertices::Vector{Int}, nodes::Vector{Node})
-
-        dN = dNmatrix(interp, local_coords)
-        J = Jmatrix(interp, local_coords, vertices, nodes, dN)
-        dNdx = dN * (inv(J)')
-
-        return dNdx
-end
-
 
 function dNmatrix(interp::LinTrigInterp, loc_coords::Vector{Float64})
     # Derivative w.r.t ξ
@@ -34,21 +24,3 @@ function dNmatrix(interp::LinTrigInterp, loc_coords::Vector{Float64})
 end
 
 
-function Jmatrix(interp::LinTrigInterp, local_coords::Vector{Float64},
-                 vertices::Vector{Int}, nodes::Vector{Node},
-                 dN::Matrix{Float64})
-
-    J = zeros(2, 2)
-
-    for row in 1:3
-        x = nodes[vertices[row]].coordinates[1]
-        y = nodes[vertices[row]].coordinates[2]
-
-        J[1, 1] += dN[row, 1] * x
-        J[1, 2] += dN[row, 1] * y
-        J[2, 1] += dN[row, 2] * x
-        J[2, 2] += dN[row, 2] * y
-    end
-
-    return J
-end
