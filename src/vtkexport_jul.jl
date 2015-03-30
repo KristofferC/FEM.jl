95ccb985e7f5e0c784a90b5bb0c4360206dd277e
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-03-30 09:48:30 +0200

    Fixed binary export

--- src/vtkexport_jul.jl
+++ src/vtkexport_jul.jl
@@ -45,26 +45,26 @@ function exportVTK(fp::FEProblem, filename, ascii::Bool=false)
                 print(fid, element.vertices.v2-1, " ");
                 print(fid, element.vertices.v3-1, "\n");
             else
-                write(fid, bswap(3))
-                write(fid, bswap(element.vertices.v1-1))
-                write(fid, bswap(element.vertices.v2-1))
-                write(fid, bswap(element.vertices.v3-1))
+                write(fid, bswap(Int32(3)))
+                write(fid, bswap(Int32(element.vertices.v1-1)))
+                write(fid, bswap(Int32(element.vertices.v2-1)))
+                write(fid, bswap(Int32(element.vertices.v3-1)))
             end
         end
     end
 
-    println(fid,"CELL_TYPES ",nr_of_elements);
+    println(fid,"\nCELL_TYPES ",nr_of_elements);
     for section in fp.sections
         for element in values(section.elements)
             if ascii
                 print(fid, 5, "\n")
             else
-                write(fid, bswap(5))
+                write(fid, bswap(Int32(5)))
             end
         end
     end
 
-#=
+
     print(fid, "\nPOINT_DATA $(length(fp.nodes))\n");
 
     println(fid, "\nVECTORS Displacement double");
@@ -95,9 +95,14 @@ function exportVTK(fp::FEProblem, filename, ascii::Bool=false)
             strain_buf[2,2] = strain[2]
             strain_buf[3,3] = strain[3]
             strain_buf[1,3] = strain[3,1] = strain[4]
-
-            print(fid, "$(strain_buf)"[2:end-1]);
-            print(fid, "\n");
+            if ascii
+                print(fid, "$(strain_buf)"[2:end-1]);
+                print(fid, "\n");
+            else
+                for strain in strain_buf
+                    write(fid, bswap(strain))
+                end
+            end
         end
     end
 
@@ -114,20 +119,16 @@ function exportVTK(fp::FEProblem, filename, ascii::Bool=false)
             stress_buf[3,3] = stress[3]
             stress_buf[1,3] = stress[3,1] = stress[4]
 
-            print(fid, "$(stress_buf)"[2:end-1]);
-            print(fid, "\n");
+            if ascii
+                print(fid, "$(stress_buf)"[2:end-1]);
+                print(fid, "\n");
+            else
+                for stress in stress_buf
+                    write(fid, bswap(stress))
+                end
+            end
         end
     end
-
-
-    #print(fid, "\nCELL_DATA $(nr_of_elements)\n");
-
-    #print(fid,"TENSORS ", strain," double\n");
-    #for section in fp.sections
-    #    matstats = section.material.matstats
-    #    end
-    #end
-    =#
 
     close(fid);
 end
