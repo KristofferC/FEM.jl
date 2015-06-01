7b15ca136d3702bf5ff70b9c85a0eef0e99195bd
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-06-02 01:33:43 +0200

    Kept working

--- src/materials/grad_mekh_jul_small.jl
+++ src/materials/grad_mekh_jul_small.jl
@@ -1,5 +1,5 @@
 
-module GradMekhModJl
+module GradMekhModJlSmall
 
 using Devectorize
 import Base.copy
@@ -16,8 +16,8 @@ const NSLIP = 2
 
 type GradMekhMS <: AbstractMaterialStatus
     n_ε_p::Vector{Float64}
-    n_k_α::Vector{Float64}
-    n_λ_α::Vector{Float64}
+    n_k::Vector{Float64}
+    n_∆λ::Vector{Float64}
     strain::Vector{Float64}
     stress::Vector{Float64}
 end
@@ -28,14 +28,14 @@ function GradMekhMS()
   GradMekhMS(zeros(9), zeros(NSLIP), zeros(NSLIP), zeros(6), zeros(6))
 end
 
-copy(matstat::GradMekhMS) = GradMekhMS(copy(matstat.n_ε_p), copy(matstat.n_k_α), copy(matstat.n_λ_α),
+copy(matstat::GradMekhMS) = GradMekhMS(copy(matstat.n_ε_p), copy(matstat.n_k), copy(matstat.n_∆λ),
                                        copy(matstat.strain), copy(matstat.stress))
 
-get_kalpha(ms::GradMekhMS, i::Int) = ms.n_k_α[i]
+get_kalpha(ms::GradMekhMS, i::Int) = ms.n_k[i]
 
 immutable GradMekh <: AbstractMaterial
     E::Float64
-    ν ::Float64
+    ν::Float64
     n::Float64
     l::Float64
     Hg::Float64
@@ -49,8 +49,8 @@ immutable GradMekh <: AbstractMaterial
 end
 
 
-function GradMekh(E, nu, n, l, Hg, Hl, m, factor,
-                  sy, tstar, angles, NSLIP)
+function GradMekh(E, ν, n, l, Hg, Hl, m,
+                  σy, tstar, angles, NSLIP)
 
     if length(angles) != NSLIP
         error("Need one angle per slip system")
@@ -58,16 +58,15 @@ function GradMekh(E, nu, n, l, Hg, Hl, m, factor,
 
     s_x_m = Vector{Vector{Float64}}()
     @devec angles[:] = angles[:] .* pi ./ 180.0
-    for i = 1:NSLIP
-        t = angles[i]
+    for α = 1:NSLIP
+        t = angles[α]
         s = [cos(t), sin(t), 0.0]
         mm = [cos(t + pi/2), sin(t + pi/2), 0.0]
         sxm_mat = s * mm'
         push!(s_x_m, M_2_V9(sxm_mat))
     end
 
-    GradMekh(E, nu, n, l Hg, Hl, m,
-             factor, sy, tstar, angles, s_x_m,
+    GradMekh(E, ν, n, l, Hg, Hl, m, σy, tstar, angles, s_x_m,
              NSLIP)
 end
 
@@ -150,101 +149,153 @@ const I = Float64[1,1,1,0,0,0,0,0,0]
 function stress(mat::GradMekh, matstat::GradMekhMS, temp_matstat::GradMekhMS,
                 κ_nl::Vector{Float64}, F1::Matrix{Float64})
 
-    ε = sym_V9!(M_2_V9(F1))
+    ε = sym_V9(M_2_V9(F1))
     ε[1] -= 1.0
     ε[2] -= 1.0
     ε[3] -= 1.0
 
+    println("ε $ε")
+
     E = mat.E
-    ν = mat.nu
+    ν = mat.ν
     s_x_m = mat.s_x_m
     tstar = mat.tstar
 
     dt = 2.000000000000000e-003 #TODO: Fix
 
     n_ε_p = matstat.n_ε_p
-    n_k_α = matstat.n_k_α
-    n_λ_α = matstat.n_λ_α
+      println("n_ε_p $n_ε_p ")
 
     ε_e = ε - n_ε_p
 
     G = E / (2*(1+ν))
     L = E*ν / ((1+ν)*(1-2*ν))
-    Mbar = L * dot(I, ε_e)*I + 2*G*ε_e
+    σ = L * dot(I, ε_e)*I + 2*G*ε_e
 
     # determine the sign of s_alpha
-    for i in 1:NSLIP
-       sxm = s_x_m[i]
-       t = dot(Mbar, sxm)
+    for α in 1:NSLIP
+       sxm_α = s_x_m[α]
+       t = dot(Mbar, sxm_α)
        if t < 0
-          @devec sxm[:] = -sxm
+          @devec sxm_α[:] = -sxm_α
        end
     end
 
     #Solve nonlinear system of equations
-    #unknowns: invFp,dlambda(ii),Φ(ii)
-    # initial guess:
-    ε_p = zeros(9)
-    λ_α = zeros(NSLIP)
-    k_α = zeros(NSLIP)
+    #unknowns: invFp,dlambda(α),Φ(α)
+
+    # Below could be used for guessing
+    #n_k = matstat.n_k
+    #n_∆λ = matstat.n_∆λ
+    @devec ε_p = n_ε_p
+    k = zeros(NSLIP)
+    ∆λ = zeros(NSLIP)
 
     unbal = typemax(Float64)
     niter = 0
-
     σ = zeros(9)
     abstol = 1e-6
+    H = 1e-7
+    max_niter = 8
+    J = zeros(NSLIP, NSLIP)
     while true
-        # För ett givet λ beräkning av obalanskrafter
-        J, R, σ, k_α, ε_p = const_unbal_dam_se_lem(mat, matstat, λ, ε, κ_nl, dt)
-
-        if niter > 8
-             error("*** No convergence in const subroutine")
-        end
-
+        # För ett givet ∆λ_α beräkning av obalanskrafter
+        R, k = compute_imbalance(mat, matstat, ∆λ, ε, κ_nl, dt)
+        println(R)
         if norm(R) < abstol * tstar
             println("Converged in $niter iterations")
             break
         else
-            ∆λ = J \ R
+            for α in 1:NSLIP
+                ∆λ[α] += H
+                Rdiff, _ = compute_imbalance(mat, matstat, ∆λ, ε, κ_nl, dt)
+                J[:, α] = (Rdiff - R) / H
+                ∆λ[α] -= H
+            end
+            if niter > max_niter
+                 error("*** No convergence in const subroutine")
+            end
+
+            d∆λ = J \ R
+             println("d∆λ $d∆λ")
             #Newton type of update, but dlambda never negative
-            @devec λ[:] = λ - ∆λ
-            for ii=1:NSLIP
-                if λ[ii] < 0
-                    λ[ii]=1.e-10
+            @devec ∆λ[:] = ∆λ - d∆λ
+             println("∆λ: $∆λ")
+            for α=1:NSLIP
+                if ∆λ[α] < 0
+                    ∆λ[α] = 1.e-10
                 end
             end
+
             niter=niter+1
         end
-    end  #end iteration on λ
+    end  #end iteration on ∆λ
 
     @devec temp_matstat.n_ε_p[:] = ε_p
-    @devec temp_matstat.n_k_α[:] = k_α
-    @devec temp_matstat.n_λ_α[:] = λ
+    @devec temp_matstat.n_k[:] = k
+    @devec temp_matstat.n_∆λ[:] = ∆λ
 
     return σ
 end
 
-function  const_unbal_dam_se_lem(mat, matstat, λ, ε, κ_nl, dt)
+function compute_imbalance(mat, matstat, ∆λ, ε, κ_nl, dt)
+    E = mat.E
+    ν = mat.ν
+    s_x_m = mat.s_x_m
+    tstar = mat.tstar
+    n_ε_p = matstat.n_ε_p
+    n_k = matstat.n_k
+    n = mat.m
+
+
+    R = zeros(NSLIP)
+    Φ = zeros(NSLIP)
+    k = zeros(NSLIP)
+
+    # εp = εp - Σ ∆λ_α s_α ⊗ m_α
+    @devec ε_p = n_ε_p
+    for α in 1:NSLIP
+        ε_p -= ∆λ[α] * s_x_m[α]
+    end
+
+    ε_e = ε - ε_p
+
+    G = E / (2*(1+ν))
+    L = E*ν / ((1+ν)*(1-2*ν))
+    # Calculate Mandel stress
+    σ = L * dot(I, ε_e)*I + 2*G*ε_e
+
+    for α=1:NSLIP
+       k[α] = n_k[α] - ∆λ[α]
+       Φ[α] = dot(σ, s_x_m[α]) - (-mat.Hl * k[α] + κ_nl[α]) - mat.σy
+        # Computation of R_\Delta_\lambda_\alpha
+       R[α] = tstar*∆λ[α] - dt * max(0, Φ[α])^n
+    end
+    return R, k
+end
+
+
+function  const_unbal_dam_se_lem(mat, matstat, ∆λ, ε, κ_nl, dt)
 
     E = mat.E
     ν = mat.nu
     s_x_m = mat.s_x_m
     tstar = mat.tstar
-    n = mat.m
     m = mat.m
     Hl = mat.Hl
 
     n_ε_p = matstat.n_ε_p
-    n_k_α = matstat.n_k_α
+    n_k = matstat.n_k
 
     J = zeros(NSLIP, NSLIP)
     R = zeros(NSLIP)
     Φ = zeros(NSLIP)
     k_α = zeros(NSLIP)
-    @devec ε_p[:] = n_ε_p
 
-    for ii in 1:NSLIP
-        ε_p -= λ[ii] * s_x_m[ii]
+    # εp = εp - Σ ∆λ_α s_α ⊗ m_α
+    @devec ε_p[:] = n_ε_p
+    for α in 1:NSLIP
+        ε_p -= ∆λ[α] * s_x_m[α]
     end
 
     ε_e = ε - ε_p
@@ -255,33 +306,33 @@ function  const_unbal_dam_se_lem(mat, matstat, λ, ε, κ_nl, dt)
     σ = L * dot(I, ε_e)*I + 2*G*ε_e
 
 
-    for ii=1:NSLIP
-       k_α [ii] = n_k_α[ii] - λ[ii]
-       Φ[ii] = dot(σ, s_x_m[ii]) - (-Hl * k_α[ii] + κ_nl[ii]) - mat.σy
+    for α=1:NSLIP
+       k_α[α] = n_k[α] - λ[α]
+       Φ[α] = dot(σ, s_x_m[α]) - (-Hl * k_α[α] + κ_nl[α]) - mat.σy
         # Computation of R_\Delta_\lambda_\alpha
-       R[ii] = tstar*λ[ii] - dt*max(0, Φ[ii])^n
+       R[α] = tstar*λ[α] - dt*max(0, Φ[α])^n
     end
 
-    for ii = 1:NSLIP, jj = 1:NSLIP
-        if Φ[ii] > 0
-            J[ii,jj]= -2 * G * dot( s_x_m[ii], dot(D, s_x_m[jj]))
+    for α = 1:NSLIP, β = 1:NSLIP
+        if Φ[α] > 0
+            J[α,β]= -2 * G * dot( s_x_m[α], dot(D, s_x_m[β]))
         else
-            J[ii,jj] = 0
+            J[α,β] = 0
         end
     end
 
-    for ii =1:NSLIP, jj = 1:NSLIP
-        if Φ[ii] > 0
-             if ii == jj
-                 J[ii,jj] -= Hl
-                 J[ii,jj] = tstar - dt*n*Φ[ii]^(n-1) * J[ii,jj]
+    for α =1:NSLIP, β = 1:NSLIP
+        if Φ[α] > 0
+             if α == β
+                 J[α,β] -= Hl
+                 J[α,β] = tstar - dt*n*Φ[α]^(n-1) * J[α,β]
              else
-                 J[ii,jj] = -dt*n * Φ[ii]^(n-1) * J[ii,jj]
+                 J[α,β] = -dt*n * Φ[α]^(n-1) * J[α,β]
              end
         else
-            J[ii,jj] = 0
-            if ii == jj
-                J[ii,jj] = tstar
+            J[α,β] = 0
+            if α == β
+                J[α,β] = tstar
             end
         end
     end
